62e383774efafb32bfb472305b742e1752c33771
Adam-Barto <adam.jbarto@gmail.com>
2023-05-20 13:18:53 -0400

    Increased visibility

--- full_name.py
+++ full_name.py
@@ -1,7 +1,9 @@
 first_name = 'Adam'
 last_name = 'Barto'
-#print(first_name * 500)
+print(first_name * 500)
 
-full_name = first_name + last_name
+full_name = first_name + " " + last_name
 
 print(full_name)
+
+print(full_name.swapcase())
